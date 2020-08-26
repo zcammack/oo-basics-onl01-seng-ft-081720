5b8245baf0eff0097a3cd43293aa4f9572b10930
Zackary Cammack <zack.cammack@gmail.com>
2020-08-26 19:04:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,20 @@ class Shoe
   end
 
   attr_reader :brand
+<<<<<<< HEAD
   attr_accessor :color, :size, :material, :condition
 
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
+=======
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+
+  def cobble
+    puts "Your shoe is as good as new!"
+>>>>>>> 3c89e06d65a8d1353d759d84c41496652ad0693d
   end
 end
